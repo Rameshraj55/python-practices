1cd9485c25e03767dd138828ed10ff2a94a910ee
Rameshraj55 <67755006+Rameshraj55@users.noreply.github.com>
2020-11-09 10:46:35 +0530

    Update int methods1.py

--- int methods1.py
+++ int methods1.py
@@ -3,17 +3,3 @@ print(a.count('b',3,7))
 text = "programming is easy"
 result = text.endswith(('programming', 'python'))
 
-# prints False
-print(result)
-
-result = text.endswith(('python', 'easy', 'java'))
-
-#prints True
-print(result)
-
-# With start and end parameter
-# 'programming is' string is checked
-result = text.endswith(('is', 'an'), 0, 14)
-
-# prints True
-print(result)
