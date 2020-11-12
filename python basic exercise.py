a376aafa8e4e813cdf72ee41b753fb2915b16a30
Rameshraj55 <67755006+Rameshraj55@users.noreply.github.com>
2020-11-12 10:20:06 +0530

    Update python basic exercise.py

--- python basic exercise.py
+++ python basic exercise.py
@@ -1,133 +1,83 @@
+"""INSTRUCTIONS:
+1.DISPLAY QUESTIONS USING questions.py module 
+2.CALL THE FUNCTIONS BY ITS FUNCTIONS NAME IN python basic exercise.py module
+"""
+import questions
+#NEW LINE
+def new_line():
+    print('\n')
+new_line()
 #1
-'''Twinkle, twinkle, little star,
-	How I wonder what you are! 
-		Up above the world so high,   		
-		Like a diamond in the sky. 
-Twinkle, twinkle, little star, 
-	How I wonder what you are'''
 def poem():
-    print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are!")
-#poem()
+    print('\nSolution:'),print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are!\n")
+
 #2
 def version():
     import sys
-    print('Version')
-    print(sys.version)
-    print('Version_info')
-    print(sys.version_info)
-#version()
+    print('\nVersion',sys.version)
+    print('Version_info',sys.version_info,end='\n\n')
 
 #3
 import datetime
 def date_time():
     current_time = datetime.datetime.now()
-    print("\nCurrent Date and Time: ",current_time)
-    print (current_time.strftime("%Y-%m-%d %H:%M:%S"))
-#date_time()
+    print("\nCurrent Date and Time: ",current_time), print (current_time.strftime("%Y-%m-%d %H:%M:%S"))
+
 #4
 from math import pi
 def area_of_circle():
-    print('\n')
-    radius_input = float(input('Radius of the Circle: '))
-    area = round(pi*(radius_input**2),4)
-    print("Area of the Circle: ",area)
-    print(f"Calculated area: {area} square units")
-#area_of_circle()
+    radius_input = float(input('Radius of the Circle: '));area = round(pi*(radius_input**2),4)
+    print("Area of the Circle: ",area),print(f"Calculated area: {area} square units\n")
+
 #5
 def lastname_firstname():
-    print('\n')
-    fname,lname= input('First name: '),input('Last name: ') 
+    fname,lname= input('First name: '),input('Last name: ')
     print(lname + " " + fname)
-#lastname_firstname()
+
 #6
 def gen_list_and_tup():
-    print('\n')
-    lis= []
-    a = input('Number to add: ')
-    if a.isnumeric() :
-        for a not in lis:
-            lis.append(a)
-            return gen_list_and_tup()
-        print(lis)
-    
-    else:
-        print('Please enter a valid number!!')
-        return gen_list_and_tup()
-    #print('Nums in list: ',lis)#print('Nums in tuple: ',tup)
-gen_list_and_tup()
+    values = (input('Enter some numbers with space: '))
+    lis = values.split(' ')
+    tup = tuple(lis)
+    set1 = set(lis)
+    set2 = set(tup) # set from tuple
+    #dict1 = dict(lis).update()
+    print(f"In List: {lis}\nIn Tuple: {tup}\nIn various data type: {set1}")
+
 #7
 def extension_return():
     ext_ip= input('Filename: ')
     ext_dot_check = ext_ip.split('.')
     print(ext_dot_check[-1])
-#extension_return()
+
 #8
 def first_last_clrs():
     clr_list= ['Red','Green','Blue','Black','White']
     print(f'First Color: {clr_list[0]} and Last color: {clr_list[-1]}')
-#first_last_clrs()
+
 #9
 def exam_schedule():
     date= input('Date: ')
     replace_dt = date.replace(',','/',3)
     print(replace_dt)
-#exam_schedule()   
+
 #10
 def n_nn_nnn():    
     n= int(input("Num: "))
     n1,n2,n3=n,(n*11),(n*111) 
     print(n1+n2+n3)
-#n_nn_nnn()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#11
+def numsuperpairs(nums):
+    dict1 = dict()
+    pairs = 0
+    for i in nums:
+        if i in dict1:
+            pairs += dict1[i]
+            dict1[i] += 1
+        else:
+            dict1[i] = 1
+    print(pairs)
 
 
 
