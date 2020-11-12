b1016fe37a1dacdf46147785c6230ae0b0a67840
Rameshraj55 <67755006+Rameshraj55@users.noreply.github.com>
2020-11-12 10:15:16 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,43 @@
+def new_line():
+    print('\n')
+new_line()
+def pb1():
+    print('''1.Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are''')
+
+def pb2():
+    print('2.Write a Python program to get the Python version you are using.')
+
+def pb3():
+    print('3. Write a Python program to display the current date and time.')
+
+def pb4():
+    print('4.Write a Python program which accepts the radius of a circle from the user and compute the area.')
+
+def pb5():
+    print("6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.")
+
+def pb6():
+    print("7. Write a Python program to accept a filename from the user and print the extension of that.")
+
+def pb7():
+    print("7. Write a Python program to accept a filename from the user and print the extension of that.")
+
+def pb8():
+    print("""8. Write a Python program to display the first and last colors from the following list. Go to the editor
+color_list = ["Red","Green","White" ,"Black"]""")
+
+def pb9():
+    print("9. Write a Python program to display the examination schedule. (extract the date from exam_st_date). ")
+
+def pb10():
+    print("10. Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.")
+
+def pb11():
+    print('''11.Given an array of integers nums.
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+Return the number of good pairs.\n''')
